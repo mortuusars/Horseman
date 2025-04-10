ef0822379bcf2bc120d3e638746bfdc267d15a46
mortuusars <mortuusars@gmail.com>
2025-04-10 22:16:43 +0300

    Fix horsebuff crash

--- common/src/main/java/io/github/mortuusars/horseman/mixin/horse_fits_in_boat/BoatMixin.java
+++ common/src/main/java/io/github/mortuusars/horseman/mixin/horse_fits_in_boat/BoatMixin.java
@@ -1,4 +1,4 @@
-package io.github.mortuusars.horseman.mixin.horse_in_boat;
+package io.github.mortuusars.horseman.mixin.horse_fits_in_boat;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
