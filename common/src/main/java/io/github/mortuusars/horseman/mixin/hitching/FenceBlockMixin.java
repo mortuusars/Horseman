c1e1840ec8722ef336655fdbd3a8dc80e85c16b4
mortuusars <mortuusars@gmail.com>
2025-04-12 01:34:18 +0300

    Fix hitching block place effects on client

--- common/src/main/java/io/github/mortuusars/horseman/mixin/hitching/FenceBlockMixin.java
+++ common/src/main/java/io/github/mortuusars/horseman/mixin/hitching/FenceBlockMixin.java
@@ -2,9 +2,13 @@ package io.github.mortuusars.horseman.mixin.hitching;
 
 import io.github.mortuusars.horseman.world.HitchableHorse;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.LeadItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.FenceBlock;
@@ -17,6 +21,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FenceBlock.class)
 public class FenceBlockMixin {
+    @Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
+    private void onUse(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand,
+                       BlockHitResult hitResult, CallbackInfoReturnable<ItemInteractionResult> cir) {
+        if (level.isClientSide
+                && player.getRootVehicle() instanceof AbstractHorse horse
+                    && horse instanceof HitchableHorse hitchableHorse
+                    && HitchableHorse.canHitch(hitchableHorse)) {
+            cir.setReturnValue(ItemInteractionResult.SUCCESS);
+        }
+    }
+
     @Inject(method = "useWithoutItem", at = @At("HEAD"), cancellable = true)
     private void onUse(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult,
                        CallbackInfoReturnable<InteractionResult> cir) {
