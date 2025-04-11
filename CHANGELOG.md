f0725ce02fc45cc2ed2acd7612a2b1f9d037ed21
mortuusars <mortuusars@gmail.com>
2025-04-11 03:26:10 +0300

    prevent rearing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,11 +3,12 @@
 ## UNRELEASED - 2025-04-10
 - Using saddle in creative mode on an untamed horse instantly tames it.
 
-**Due to an agreement with Horse Buff mod author, Horseman will not include Horse Buff's features.**
+**Due to archiving of Horse Buff mod and an agreement with its author, Horseman will now include Horse Buff's features.**
 
 Features ported/adapted from Horse Buff:
 - Horses now fit in boats. Their hitbox gets a bit smaller to make breaking the boat easier. 
 - Saddled horse will stay near the position where player left it.
+- Prevent rearing when riding (no bucking).
 
 ## 1.2.1 - 2025-04-10
 - Fixed loading crash with Horse Buff.
---
+++ common/src/main/java/io/github/mortuusars/horseman/mixin/no_rearing/AbstractHorseMixin.java
@@ -0,0 +1,27 @@
+package io.github.mortuusars.horseman.mixin.no_rearing;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import io.github.mortuusars.horseman.Config;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = AbstractHorse.class, priority = 950)
+public abstract class AbstractHorseMixin {
+    @Shadow public abstract boolean isJumping();
+    @Shadow public abstract boolean isTamed();
+    @Shadow public abstract LivingEntity getControllingPassenger();
+
+    @ModifyReturnValue(method = "isStanding", at = @At("RETURN"))
+    private boolean isStanding(boolean original) {
+        if (Config.Server.HORSE_PREVENT_REARING_WHEN_RIDING.get()
+            && !isJumping()
+            && isTamed()
+            && getControllingPassenger() != null) {
+            return false;
+        }
+        return original;
+    }
+}
